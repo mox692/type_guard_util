30847446efac5bf4695ac12621b442818183bb9c
mox692 <55653825+mox692@users.noreply.github.com>
2021-04-11 16:25:33 +0900

    imple type is

---
+++ src/TypeIs.ts
@@ -0,0 +1,10 @@
+/**
+* @template T The type you want to know whether @arg's type is T.
+* @template W (optional) The type to which @T extends.
+* @param arg The arg which you want check type.
+* @param prop The prop that is unique in arg Types.
+* @return whether @prop exist in @arg.
+*/
+export const TypeIs = <T extends W, W = any,>(arg:W, prop: any): prop is T => {
+    return prop in arg
+}
